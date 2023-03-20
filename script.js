cad46391f9982a15ef675e93692a63e8faf7ef53
CodyConck <codyconck@gmail.com>
2023-03-20 13:50:40 -0500

    returning multiple divs now, but only returning one movies info

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 const searchBox = document.getElementById("movie-search-box");
 const resultGrid = document.getElementById("result-grid");
-const searchBtn = document.getElementById("search-btn");
+//const searchBtn = document.getElementById("search-btn");
 
+// load movies from API
 async function fecthMovieData(searchTerm) {
   // console.log(searchTerm);
   const URL = `http://www.omdbapi.com/?s=${searchTerm}&page1&apikey=9c901ace`;
@@ -27,33 +28,23 @@ function findMovies() {
 }
 
 function displayMovieDetails(movies) {
-  // console.log(movies);
-  resultGrid.innerHTML = "";
+  //console.log(movies);
+  //resultGrid.innerHTML = "";
   for (let index = 0; index < movies.length; index++) {
     let movieDisplay = document.createElement("div");
     //console.log(movieDisplay);
     movieDisplay.dataset.id = movies[index].imdbID;
     movieDisplay.classList.add("result-container");
 
-    // movieDisplay.innerHTML = `
-    // <div class="movie-poster">
-    //          <img src="${movies[index].Poster}" alt="movie poster" />
-    //        </div>
-    //        <div class="movie-info">
-    //          <h3 class="movie-title">${movies[index].Title}</h3>
-    //          <ul class="additional-movie-info">
-    //            <li class="Year">Year: ${movies[index].Year}</li>
-    //          </ul>
-    //       </div>
-    // `;
     resultGrid.appendChild(movieDisplay);
+    //console.log(movieDisplay);
   }
   loadAdditionalDetails();
 }
 
 function loadAdditionalDetails() {
   const searchedMovies = resultGrid.querySelectorAll(".result-container");
-  // console.log(searchedMovies);
+  //console.log(searchedMovies);
   searchedMovies.forEach(async (movie) => {
     let movieId = movie.dataset.id;
     // console.log(movieId);
@@ -68,85 +59,29 @@ function loadAdditionalDetails() {
 }
 
 function displayAdditionalDetails(details) {
-  // console.log(details);
-
+  //console.log(details);
   resultGrid.innerHTML = "";
 
   let movieInfoArray = Object.values(details);
-  console.log(movieInfoArray.length);
+  //console.log(movieInfoArray);
   for (let index = 0; index < movieInfoArray.length; index++) {
     let display = document.createElement("div");
-    // console.log(display);
+    console.log(display);
     display.classList.add("result-container");
 
-    resultGrid.appendChild(display);
-
-    resultGrid.innerHTML = `
+    display.innerHTML = `
     <div class="movie-poster">
-               <img src="${details.Poster}" alt="movie poster" />
-             </div>
-             <div class="movie-info">
-               <h3 class="movie-title">${details.Title}</h3>
-                 <ul class="additional-movie-info">
-                   <li class="Year">Year: ${details.Year}</li>
-                   <li class="rated">Rated: ${details.Rated}</li>
-                   <li class="released">Released: ${details.Released}</li>
-                 </ul>
-             </div>
+    <img src="${details.Poster}" alt="movie poster" />
+    </div>
+    <div class="movie-info">
+    <h3 class="movie-title">${details.Title}</h3>
+    <ul class="additional-movie-info">
+    <li class="Year">Year: ${details.Year}</li>
+    <li class="rated">Rated: ${details.Rated}</li>
+    <li class="released">Released: ${details.Released}</li>
+    </ul>
+    </div>
     `;
+    resultGrid.appendChild(display);
   }
-  // {
-
-  //   //console.log(details[title]);
-  //   //console.log(`There are ${details[movieKey]} ${movieKey}`);
-  //   // let additionalDetails = details[movieKey];
-  //   // console.log(additionalDetails);
-  //   let finalDisplay = document.createElement("div");
-  //   //console.log(finalDisplay);
-  //   finalDisplay.dataset.id = details[title];
-  //   finalDisplay.classList.add("result-container");
-
-  //   resultGrid.appendChild(finalDisplay);
-
-  //   resultGrid.innerHTML = `
-  //   <div class="movie-poster">
-  //             <img src="${details.Poster}" alt="movie poster" />
-  //           </div>
-  //           <div class="movie-info">
-  //             <h3 class="movie-title">${details.Title}</h3>
-  //               <ul class="additional-movie-info">
-  //                 <li class="Year">Year: ${details.Year}</li>
-  //                 <li class="rated">Rated: ${details.Rated}</li>
-  //                 <li class="released">Released: ${details.Released}</li>
-  //               </ul>
-  //           </div> `;
-  // });
 }
-
-// function loadAdditionalDetails() {
-//   const searchedMovies = resultGrid.querySelectorAll(".result-container");
-//   searchedMovies.forEach((movie) => {
-//     //console.log(movie.dataset.id);
-//     searchBtn.addEventListener("click", async () => {
-//       searchBox.value = "";
-//       const result = await fetch(
-//         `http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9c901ace`
-//       );
-//       const additionalDetails = await result.json();
-//       displayAdditionalDetails(additionalDetails);
-//     });
-//   });
-// }
-
-// function displayAdditionalDetails(details) {
-//   resultGrid.innerHTML = `
-//   <div class="movie-info">
-//   <h3 class="movie-title">${details.Title}</h3>
-//   <ul class="additional-movie-info">
-//     <li class="rated">Rated: ${details.Rated}</li>
-//     <li class="released">Released: ${details.Released}</li>
-//   </ul>
-// </div>
-
-//   `;
-// }
