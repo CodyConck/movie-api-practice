eba741e35db7ff8bc32af8755e36105938711f95
CodyConck <codyconck@gmail.com>
2023-10-04 13:14:34 -0500

    added async functionality for the additional movie data fetch to avoid calling it in a loop. updated some styling and created placeholder and images for when api returns N/A, added clear movie results to removie previous movies displayed info

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const searchBox = document.getElementById("movie-search-box");
 const resultGrid = document.getElementById("result-grid");
 
+function clearMovieResults() {
+  resultGrid.innerHTML = "";
+}
+
 // load movies from API
 async function fecthMovieData(searchTerm) {
   //console.log(searchTerm);
@@ -9,8 +13,11 @@ async function fecthMovieData(searchTerm) {
   const res = await fetch(`${URL}`);
   const movieData = await res.json();
   //console.log(movieData);
-  if (movieData.Response == "True") displayMovieDetails(movieData.Search);
-  //console.log(movieData.Search);
+  if (movieData.Response == "True") {
+    clearMovieResults();
+    displayMovieDetails(movieData.Search);
+    //console.log(movieData.Search);
+  }
 }
 
 //trim and store searched term in let variable
@@ -26,39 +33,75 @@ function findMovies() {
   }
 }
 
-function displayMovieDetails(details) {
+async function fetchAdditionalMovieData(imdbID) {
+  const res = await fetch(
+    `http://www.omdbapi.com/?i=${imdbID}&apikey=9c901ace`
+  );
+  const data = await res.json();
+  return data;
+}
+
+async function displayMovieDetails(details) {
   //console.log(details);
+
+  function getPlaceholderText(value, placeholder) {
+    return value !== "N/A" && value !== "" ? value : placeholder;
+  }
 
   //loop thru details and create new div to display each movie returned
   for (let i = 0; i < details.length; i++) {
     let display = document.createElement("div");
 
-    //get imdbID and use that to fetch more movie data
-    let movieId = details[i].imdbID;
-    //console.log(movieId);
-    fetch(`http://www.omdbapi.com/?i=${movieId}&apikey=9c901ace`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        //console.log(data);
-        display.classList.add("result-container");
+    let poster = details[i].Poster;
+    let genericImageUrl = `https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg`;
+    if (poster === "N/A" || poster === "") {
+      poster = genericImageUrl;
+    }
 
-        display.innerHTML = `
-          <div class="movie-poster">
-          <img src="${data.Poster}" alt="movie poster" />
-          </div>
-          <div class="movie-info">
-          <h3 class="movie-title">${data.Title}</h3>
+    //get imdbID and use that to fetch more movie data
+    let imdbID = details[i].imdbID;
+    console.log(imdbID);
+    try {
+      const additionalData = await fetchAdditionalMovieData(imdbID);
+
+      let year = getPlaceholderText(additionalData.Year, "No Year Listed.");
+      let rated = getPlaceholderText(additionalData.Rated, "No Rating Listed.");
+      let released = getPlaceholderText(
+        additionalData.Released,
+        "No Release Date Listed."
+      );
+      let plot = getPlaceholderText(
+        additionalData.Plot,
+        "No Plot Info Listed."
+      );
+      let runtime = getPlaceholderText(
+        additionalData.Runtime,
+        "No Runtime Listed."
+      );
+
+      display.classList.add("result-container");
+
+      display.innerHTML = `
+        <div class="movie-poster">
+          <img src="${poster}" alt="movie poster" />
+        </div>
+        <div class="movie-info">
+          <h3 class="movie-title">${details[i].Title}</h3>
           <ul class="additional-movie-info">
-          <li class="Year">Year: ${data.Year}</li>
-          <li class="rated">Rated: ${data.Rated}</li>
-          <li class="released">Released: ${data.Released}</li>
-          <li class="runtime"> Runtime:${data.Runtime}</li>
+            <li class="Year">Year: ${year}.</li>
+            <li class="rated">Rated: ${rated}</li>
+            <li class="released">Released: ${released}.</li>
+            <li class="runtime">Runtime: ${runtime}</li>
           </ul>
-          </div>
-          `;
-        resultGrid.appendChild(display);
-      });
+          <h3 class="plot">${plot}</h3>
+        </div>
+      `;
+
+      resultGrid.appendChild(display);
+    } catch (error) {
+      console.error(
+        `Error fetching additional data for IMDb ID ${imdbID}: ${error}`
+      );
+    }
   }
 }
